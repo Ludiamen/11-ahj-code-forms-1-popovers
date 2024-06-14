f0136f702d526f657dc9e75b1a750020a66dd546
Ludiamen <maxvas1001@gmail.com>
2024-06-14 17:43:22 +0300

    fix puppeteer settings

--- src/js/__tests__/puppeteer.test.js
+++ src/js/__tests__/puppeteer.test.js
@@ -29,15 +29,35 @@
 //   await browser.close();
 // });
 
-const puppeteer = require('puppeteer');
+// const puppeteer = require('puppeteer');
+
+// test('popover toggles on click', async () => {
+//   const browser = await puppeteer.launch({
+//     headless: true,
+//     args: ['--disable-features=NetworkService'],
+//   });
+//   const page = await browser.newPage();
+//   await page.goto('http://localhost:9000');
+
+//   const button = await page.$('#popover-btn');
+//   await button.click();
+
+//   const popoverVisible = await page.$eval('.popover', (el) => el.classList.contains('show'));
+//   expect(popoverVisible).toBe(true);
+
+//   await browser.close();
+// });
+const puppeteer = require('puppeteer-core');
 
 test('popover toggles on click', async () => {
   const browser = await puppeteer.launch({
     headless: true,
     args: ['--disable-features=NetworkService'],
+    executablePath: '/usr/bin/firefox',
   });
+
   const page = await browser.newPage();
-  await page.goto('http://localhost:9000');
+  await page.goto('http://localhost:9000'); // Замените на ваш URL
 
   const button = await page.$('#popover-btn');
   await button.click();
@@ -47,3 +67,4 @@ test('popover toggles on click', async () => {
 
   await browser.close();
 });
+
