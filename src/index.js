b00cad70bcd14dfcf2662b7fda1c461e37b2e27a
Ludiamen <maxvas1001@gmail.com>
2024-06-17 22:44:57 +0300

    fix style.css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import Popover from './js/popover';
-import './styles/style.css';
+import './css/style.css';
 
 const button = document.getElementById('popover-btn');
 const popover = new Popover(button, 'Popover title', 'And here\'s some amazing content. It\'s very engaging. Right?');
